774dc1049a78b4d47a378e0625d73d44f0acd46f
MarkianGit <markian0204@gmail.com>
2025-10-13 00:15:29 +0300

    1.5 start

---
+++ Lessons/Lesson1/lesson1_3/lesson1_3.js
@@ -0,0 +1,7 @@
+{
+    let a = 'hello okten';
+    var b = 'hello world';
+}
+console.log(b);
+console.log(a);
+
---
+++ Lessons/Lesson1/lesson1_4/lesson1_4.js
@@ -0,0 +1,9 @@
+let a = 'hello okten'
+a = '5'
+console.log(typeof a)
+
+let b = 100500
+console.log(b)
+
+let c=true
+console.log(c)
