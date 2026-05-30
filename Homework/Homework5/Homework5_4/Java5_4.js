f18c6b6b79c4e11ac51a70d42ff93adee6e811c0
MarkianGit <markian0204@gmail.com>
2026-05-30 22:55:00 +0300

    Homework 5.5

---
+++ Homework/Homework5/Homework5_4/Java5_4.js
@@ -0,0 +1,12 @@
+// #SIdMd0hQ
+//
+// – створити функцію, яка приймає масив та виводить кожен його елемент
+
+let qwe = function (array ) {
+    for ( const item of array ) {
+        console.log(item);
+    }
+}
+qwe ([1,2,3,4])
+
+
