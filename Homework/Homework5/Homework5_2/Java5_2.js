4dd4046367ddb0663ed861044122b45629672081
MarkianGit <markian0204@gmail.com>
2026-05-30 22:55:04 +0300

    Homework 5.5

---
+++ Homework/Homework5/Homework5_2/Java5_2.js
@@ -0,0 +1,14 @@
+//#ETGAxbEn8l
+//
+// – створити функцію, яка обчислює та повертає площу кола з радіусом r
+
+let radius = function ( r) {
+    
+    
+    let p=3.14;
+    
+    return p * r**2;
+
+}
+
+console.log(radius (5));
