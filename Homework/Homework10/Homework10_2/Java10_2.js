7a503411d55581a133c6d12b2b7e162d48849c65
MarkianGit <markian0204@gmail.com>
2026-06-23 14:12:43 +0300

    Homework 11

---
+++ Homework/Homework10/Homework10_2/Java10_2.js
@@ -0,0 +1,21 @@
+// створити інпут, який приймає вік людини, та кнопку, яка підтверджує дію. При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік, чи менше він за 18, та повідомити про це користувача
+
+const ageInput = document.getElementById('age-input');
+const checkBtn = document.getElementById('check-btn');
+const resultMessage = document.getElementById('result-message');
+
+checkBtn.addEventListener('click', function() {
+    const age = parseInt(ageInput.value);
+    
+    resultMessage.classList.add('color');
+
+
+
+    if (age >= 18) {
+        resultMessage.textContent = "Ви повнолітній(я). Доступ дозволено!";
+        resultMessage.style.color = "green";
+    } else {
+        resultMessage.textContent = "+";
+        resultMessage.style.color = "red";
+    }
+});
